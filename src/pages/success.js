9ff5134d7bd13f865b7f24ef8617806822ff2d25
AkshayPatel <AKSHAYPATEL2727@GMAIL.COM>
2022-10-28 19:32:14 -0400

    Final 3

--- src/pages/success.js
+++ src/pages/success.js
@@ -21,7 +21,6 @@ const Success = () => {
                 been shipped, if you would like to check the status of your order(s)
                 please press the link below.
             </p>
-            {/* <button onClick={() => router.push("/orders")} className="button mt-8 ">Go to my orders</button> */}
          </div>
         </main>
     </div>
